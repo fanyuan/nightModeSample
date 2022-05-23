013576efb2f4d132e7152b1a31143ed24e8fa668
fly <785114545@qq.com>
2022-05-23 14:31:55 +0800

    项目初始化

---
+++ app/src/main/java/com/sample/nightmodesample/MainActivity.java
@@ -0,0 +1,35 @@
+package com.sample.nightmodesample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void dayMode(View view) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
+    public void nightMode(View view) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    }
+
+    public void autoSwitch(View view) {
+        boolean flag = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO;
+        Log.d("flag","flag = " + flag + "  AppCompatDelegate.getDefaultNightMode() = " + AppCompatDelegate.getDefaultNightMode() );
+        if(flag || AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_UNSPECIFIED){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+}
